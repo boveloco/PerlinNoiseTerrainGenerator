be140e1ab0bb6ce9bc2f509d93aa1a3e8b022461
boveloco <boveloco@gmail.com>
2016-11-17 15:03:07 -0200

    parser 100%

---
+++ src/br/pucpr/cg/Parser.java
@@ -0,0 +1,41 @@
+package br.pucpr.cg;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import static java.lang.Math.toIntExact;
+
+public class Parser {
+	
+	private final static String PATH = "config.json";
+	public int MSF;
+	public int TTH;
+	public int TMV;
+	public int TMD;
+	JSONParser parser; 
+	
+	public Parser(){
+		parser = new JSONParser();
+		MSF = -1;
+		TTH = -1;
+		TMV = -1;
+		TMD = -1;
+	}
+	
+	public Parser parser() throws FileNotFoundException, ParseException, IOException{
+		JSONObject json = (JSONObject) parser.parse(new FileReader(PATH));
+
+	    MSF = toIntExact((long)json.get("MAX_SMOOTH_FILTER"));
+		TTH = toIntExact((long)json.get("TERRAIN_TRESHOLD"));
+	    TMV = toIntExact((long)json.get("TERRAIN_MIN_VALUE"));
+	    TMD = toIntExact((long)json.get("TERRAIN_MAX_DIFFERENCE"));
+	    System.out.println(MSF + " - " +  TTH + " - " + TMV + " - " +  TMD);
+	    return this;
+	}
+	
+}
